c844f82c4e0d3f0cb89079bd42055ef21e9a990f
Andre Castellanos <acastellanosaldama@gmail.com>
2020-12-11 22:04:11 -0600

    Interfaces Completed

---
+++ C#-Intermediate/Interfaces/InterfacesAndExtensibility/InterfacesAndExtensibility/DbMigrator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace InterfacesAndExtensibility
+{
+    public class DbMigrator
+    {
+        private readonly ILogger _logger;
+        public DbMigrator(ILogger logger)
+        {
+            _logger = logger;
+        }
+        public void Migrate()
+        {
+            _logger.LogInfo("Migration started at {0}" + DateTime.Now);
+
+            //Details of migrating the database
+
+            _logger.LogInfo("Migration ended at {0}" + DateTime.Now);
+        }
+    }
+}
---
+++ C#-Intermediate/Interfaces/InterfacesAndExtensibility/InterfacesAndExtensibility/FileLogger.cs
@@ -0,0 +1,30 @@
+using System.IO;
+
+namespace InterfacesAndExtensibility
+{
+    public class FileLogger : ILogger
+        {
+            private readonly string _path;
+            public FileLogger(string path)
+            {
+                _path = path;
+            }
+            public void LogError(string message)
+            {
+                Log(message, "ERROR");
+            }
+
+            public void LogInfo(string message)
+            {
+                Log(message, "INFO");
+            }
+            
+            public void Log(string message, string messageType)
+            {
+                using(StreamWriter streamWriter = new StreamWriter(_path, true))
+                {
+                    streamWriter.WriteLine(messageType + ": " + message);
+                }
+            }
+        }
+}
